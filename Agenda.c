dd9574c9be1f2ddd0dcee97fc2a74972c90f8b56
Sr. Rafael Cardoso <rafaelcsadm@hotmail.com>
2023-06-08 19:59:10 -0300

    Update Agenda.c
    separado em subrotina

--- Agenda.c
+++ Agenda.c
@@ -2,86 +2,84 @@
 #include <stdlib.h>
 #include <locale.h>
 
-typedef struct agenda{
-char nome[30], tel[20], email[100];
-int cod;
-}cadastro;
+typedef struct agenda {
+    char nome[30], tel[20], email[100];
+    int cod;
+} cadastro;
 
-int main(){
-    
-    int menu, count = 1, lista;
-    struct agenda cadastro[6];
+void inserirCadastro(cadastro agenda[], int *count) {
+    if (*count <= 5) {
+        printf("Novo Cadastro\n\n");
+        printf("Cod.: %d\n", *count);
+
+        printf("Nome: ");
+        fgets(agenda[*count].nome, sizeof(agenda[*count].nome), stdin);
+        fflush(stdin);
+
+        printf("Tel.: ");
+        fgets(agenda[*count].tel, sizeof(agenda[*count].tel), stdin);
+        fflush(stdin);
+
+        printf("E-mail: ");
+        fgets(agenda[*count].email, sizeof(agenda[*count].email), stdin);
+        fflush(stdin);
+
+        printf("\n");
+        printf("Cadastro Realizado!\n\n");
+
+        (*count)++;
+    } else {
+        printf("Agenda Lotada!\n\n");
+    }
+}
+
+void mostrarCadastros(cadastro agenda[], int count) {
+    if (count == 1) {
+        printf("Agenda Vazia!\n");
+    } else {
+        printf("Lista de Cadastros\n\n");
+
+        for (int lista = 1; lista < count; lista++) {
+            printf("Cod.: %d\n", lista);
+            printf("Nome: %s", agenda[lista].nome);
+            printf("Tel.: %s", agenda[lista].tel);
+            printf("E-mail: %s\n", agenda[lista].email);
+        }
+    }
+}
+
+int main() {
+    int menu, count = 1;
+    cadastro agenda[6];
     
     setlocale(LC_ALL, "Portuguese");
-    
-    do{
-		printf("Digite a opção desejada\n\n");
-	    printf("1 - Inserir Novo Cadastro\n");
-	    printf("2 - Mostrar Todos os Cadastros\n");
-	    printf("0 - Encerrar\n\n");
-		scanf("%d", &menu, stdin);
-		fflush(stdin);
-		printf("\n");
-		
-		system("cls");
-		
-		switch(menu){
-			case 1:
-				if(count <= 5){
-					printf("Novo Cadastro\n\n");
-					printf("Cod.: %d\n", count);
-				
-					printf("Nome: ");
-					fgets(cadastro[count].nome, 30, stdin);
-					fflush(stdin);
-				
-					printf("Tel.: ");
-					fgets(cadastro[count].tel, 20, stdin);
-					fflush(stdin);
-				
-					printf("E-mail: ");
-					fgets(cadastro[count].email, 100, stdin);
-					fflush(stdin);
-					
-					printf("\n");
-					printf("Cadastro Realizado!\n\n");
-				
-					count++;
-					
-					system("pause");
-					system("cls");
-						
-				} else {
-					printf("Agenda Lotada!\n\n");
-				}					
-			break;
-			
-			case 2:
-				if(count == 1){
-					printf("Agenda Vazia!\n");
-				} else {
-					printf("Lista de Cadastros\n\n");
-					
-					for(lista=1; lista<count; lista++){
-						printf("Cod.: %d\n", lista);
-						printf("Nome: %s", cadastro[lista].nome);
-						printf("Tel.: %s", cadastro[lista].tel);
-						printf("E-mail: %s\n", cadastro[lista].email);
-					}
-				}
-				
-				system("pause");
-				system("cls");				
-			break;
-			
-			case 0:
-			break;
-			
-			default:
-				printf("Opção Inválida!\n\n");	
-		}
-		
-	}while(menu != 0);
-	
-	return (0);
+
+    do {
+        printf("Digite a opÃ§Ã£o desejada\n\n");
+        printf("1 - Inserir Novo Cadastro\n");
+        printf("2 - Mostrar Todos os Cadastros\n");
+        printf("0 - Encerrar\n\n");
+        scanf("%d", &menu);
+        fflush(stdin);
+        printf("\n");
+
+        switch (menu) {
+            case 1:
+                inserirCadastro(agenda, &count);
+                break;
+
+            case 2:
+                mostrarCadastros(agenda, count);
+                break;
+
+            case 0:
+                break;
+
+            default:
+                printf("OpÃ§Ã£o InvÃ¡lida!\n\n");
+        }
+
+    } while (menu != 0);
+
+    return 0;
 }
